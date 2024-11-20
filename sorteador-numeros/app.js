b6c0aecee8992d90f08653de6b29fd5cc8f51c99
dionatadejesus <dionatasdejesus@hotmail.com>
2024-11-20 08:10:29 -0300

    inclusão de "if (Intervalo < quantidade)"

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,19 +2,25 @@ function sortear() {
   let quantidade = parseInt(document.getElementById("quantidade").value);
   let de = parseInt(document.getElementById("de").value);
   let ate = parseInt(document.getElementById("ate").value);
-
+  let Intervalo = ate - de;
   let sorteados = [];
   let numero;
 
   if (de > ate) {
     alert("O número FINAL deve ser Menor que o número inicial!");
+    limparCampos();
   } else {
-    for (let i = 0; i < quantidade; i++) {
-      numero = obterNumeroAleatorio(de, ate);
-      while (sorteados.includes(numero)) {
+    if (Intervalo < quantidade) {
+      alert("A QUANTIDADE escolhida deve ser menor que o INTERVALO desejado!");
+      limparCampos();
+    } else {
+      for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numero)) {
+          numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
       }
-      sorteados.push(numero);
     }
   }
 
@@ -41,6 +47,17 @@ function alterarStatusBotao() {
 }
 
 function reiniciar() {
+  quantidade.value = "";
+  de.value = "";
+  ate.value = "";
+  resultado.innerHTML = `
+    <label class="texto__paragrafo" id="numerosSorteados">
+      Números sorteados: nenhum até agora
+    </label>`;
+  alterarStatusBotao();
+}
+
+function limparCampos() {
   quantidade.value = "";
   de.value = "";
   ate.value = "";
